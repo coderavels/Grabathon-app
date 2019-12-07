718e41ce107ddf74cc62765c67083a9e8972e07c
Himanshu Tanwar <heisfullstacked@gmail.com>
2019-12-07 23:50:41 +0530

    added bootstrap, react-router, eslint

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return (
+		<header className="App-header">
+    Grab
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Home() {
+	return (
+		<Button variant="primary" size="sm" type="button" href="">Check for tasks</Button>
+	);
+}
+
+export default Home;
